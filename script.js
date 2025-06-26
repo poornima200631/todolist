5dd08ce9cd2e1e8277fe823f14d8e0c111688cd2
poornima200631 <poornimatewatia48@gmail.com>
2025-06-27 00:52:21 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+var i=0;
+var completed=0;
+function addlist(){
+    i++;
+    const val=document.getElementById("b").value;
+    const li = document.createElement("li");
+    li.textContent = val;     
+    li.classList.add("js");
+    document.getElementById("task").appendChild(li);
+    const tick = document.createElement("input");
+    tick.type = "checkbox";
+    
+    li.appendChild(tick);    
+    document.getElementById("b").value = "";
+    document.getElementById("d").innerHTML = `Total Task: ${i} &nbsp;&nbsp;&nbsp;Completed Task: 0`;
+    tick.addEventListener("change", () => {
+        if (tick.checked) {
+          completed++;
+        }else{
+            completed--;
+        }
+       count();
+      });
+      
+     
+}
+function count() {
+    document.getElementById("d").innerHTML = `Total Task: ${i} &nbsp;&nbsp;&nbsp; Completed Task: ${completed}`;
+  }
+  
